17741ecd4248070a27aeb3466c389c214f87dbd2
eliseo531 <eliseop12132@gmail.com>
2026-06-18 12:38:53 -0400

    Usar AI Service con HTTPS

--- frontend/ferreteria-web/src/app/core/services/ai.service.ts
+++ frontend/ferreteria-web/src/app/core/services/ai.service.ts
@@ -6,7 +6,7 @@ import { Injectable, inject } from '@angular/core';
 })
 export class AiService {
   private http = inject(HttpClient);
-  private apiUrl = 'http://167.172.134.57:8002/ai';
+  private apiUrl = 'https://sw2-ferreteria.duckdns.org/ai';
 
   classifyProduct(file: File) {
     const formData = new FormData();
